2148459e9cfb41f0ebe72d5555969b1382d8c24b
Кирилл Курко <kirillkurko2609@gmail.com>
2020-04-24 04:21:39 +0300

    Person class implemented

--- OOP_6/Person.cpp
+++ OOP_6/Person.cpp
@@ -1,9 +1,38 @@
-//
-//  Person.cpp
-//  OOP_6
-//
-//  Created by Кирилл Курко on 4/23/20.
-//  Copyright © 2020 Николай Курко. All rights reserved.
-//
-
 #include "Person.hpp"
+
+using namespace std;
+
+Person::Person(Fullname fullname, int age) : fullname(fullname), age(age) {}
+
+Person::Person(const Person& other) {
+    fullname = other.fullname;
+    age = other.age;
+}
+
+Fullname Person::getFullname() const {
+    return fullname;
+}
+
+int Person::getAge() const {
+    return age;
+}
+
+void Person::setFullname(Fullname fullname) {
+    this->fullname = fullname;
+}
+
+void Person::setAge(int age) {
+    this->age = age;
+}
+
+Person& Person::operator= (const Person& other) {
+    fullname = other.fullname;
+    age = other.age;
+    return *this;
+}
+
+
+void Person::printInformation() const {
+    cout << "Fullname: " << fullname << endl <<
+            "Age: " << age << endl;
+}
--- OOP_6/Person.hpp
+++ OOP_6/Person.hpp
@@ -6,7 +6,20 @@
 class Person {
 protected:
     Fullname fullname;
+    int age;
+public:
+    Person(Fullname fullname = Fullname(), int age = 0);
+    Person(const Person& other);
     
+    Fullname getFullname() const;
+    int getAge() const;
+    
+    void setFullname(Fullname fullname);
+    void setAge(int age);
+    
+    Person& operator= (const Person& other);
+    
+    void printInformation() const;
 };
 
 #endif
