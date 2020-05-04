89ea265a4ee101e54bd52dbef6a956e25b8d25c9
Кирилл Курко <kirillkurko2609@gmail.com>
2020-05-04 09:56:47 +0300

    File changes

--- OOP_6/Headers/Utilities/CreateFunctions.hpp
+++ OOP_6/Headers/Utilities/CreateFunctions.hpp
@@ -6,15 +6,7 @@
 #include <queue>
 #include "../Classes/Clinic.hpp"
 #include "../Utilities/Date.hpp"
-#include "../Classes/Doctor.hpp"
 
 Clinic CreateClinic();
-std::vector<Doctor> CreateDoctors();
-Doctor CreateDoctor();
-std::queue<Person> CreatePatients();
-Person CreatePerson();
-Fullname CreateFullname();
-Date CreateDate();
-Time CreateTime();
 
 #endif
